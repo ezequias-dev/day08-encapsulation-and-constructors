5d638ed24321e47f3362f6b4f10524dc970ea466
ezequias-dev <ezequiasd30@gmail.com>
2026-03-23 10:50:20 -0300

    fix: corrige os arquivos do exercicio 01

--- src/exercises/Exercise01.java
+++ src/exercises/Exercise01.java
@@ -6,3 +6,33 @@ public class Exercise01 {
         user.showUserSummary();
     }
 }
+
+class SystemUser {
+    private String fullName;
+    private String email;
+    private boolean active;
+
+    public SystemUser(String fullName, String email, boolean active) {
+        this.fullName = fullName;
+        this.email = email;
+        this.active = active;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void showUserSummary() {
+        System.out.println("Name: " + getFullName());
+        System.out.println("Email: " + getEmail());
+        System.out.println("Active: " + isActive());
+    }
+}
